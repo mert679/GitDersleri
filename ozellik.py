02a478c78cc3b7d27080dbed1af0449409d14580
mert679 <mertaltintas78@hotmail.com>
2025-02-13 22:28:26 +0300

    Merge bilgisi eklendi

--- ozellik.py
+++ ozellik.py
@@ -1,3 +1,6 @@
 # git branch (Mevcut branchleri gösterme)
 # git branch branch_name
 # git checkout branch_name
+
+# branch birleştirmesi merge komutu ile yapma
+# git merge branch_name(birleştirmek istediğim branch ismi.)
