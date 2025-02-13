4873147aecf3ab0898c9607ac7f5c0c99cbb98a7
mert679 <mertaltintas78@hotmail.com>
2025-02-13 22:29:39 +0300

    yeni bilgi eklendi

--- deneme.py
+++ deneme.py
@@ -3,5 +3,5 @@
 # git add . (Aybı dizindeki değişiklik yapılan file ları ekleme)
 # git commit -m "İlk commit atıldı.(deneme.py file eklendi)"
 # git log
-#.git
+#.git gizli bir şekilde eklendi.
 # git status değişitirlilen veya eklene dosyaları takibi için.
