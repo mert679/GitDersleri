834034b241b3982bbf68b8ccfdb91375113789b8
mert679 <mertaltintas78@hotmail.com>
2025-02-13 21:46:18 +0300

    İlk commit atıldı.(deneme.py file eklendi

---
+++ deneme.py
@@ -0,0 +1,7 @@
+# git init 
+# git add dosyanın_adı(eklemek istediğimiz yada değişiklik yapılan kodun dosyasını)
+# git add . (Aybı dizindeki değişiklik yapılan file ları ekleme)
+# git commit -m "İlk commit atıldı.(deneme.py file eklendi)"
+# git log
+#.git
+# git status değişitirlilen veya eklene dosyaları takibi için.
